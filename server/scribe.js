bcb40dc917341501581fcd0b6c41ce11b01fccaf
Cassels-null <42cassels@gmail.com>
2019-05-09 22:18:44 -0400

    Connect server to movie API

---
+++ server/scribe.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const API_KEY = require('./../env.js');
+
+var scribe = {
+    getGenres : (req, res)=>{
+        axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key='+API_KEY)
+        .then((result)=>{
+            res.send(result.data);
+        })
+        .catch((err)=>{
+            console.log("ERROR at scribe.getGenres: "+err)
+        })
+    },
+    getMovies : (req, res)=>{
+        axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=populerity.asc&vote_count=2&with_genres='+req.headers.genre_id+'&api_key='+API_KEY)
+        .then((result)=>{
+            res.send(result.data);
+        })
+        .catch((err)=>{
+            console.log("ERROR at scribe.getGenres: "+err)
+        })
+    }
+}
+
+module.exports = scribe;
