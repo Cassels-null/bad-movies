94d93e3c926c219f3167573d4be712f753a648d7
Cassels-null <42cassels@gmail.com>
2019-05-10 02:34:15 -0400

    Add sidebar

---
+++ client/src/sideBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+
+//react setup
+class Sidebar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+
+    render(){
+        return(
+            <div id="sidebar">meep meep!</div>
+        )
+    }
+}
+
+export default Sidebar
