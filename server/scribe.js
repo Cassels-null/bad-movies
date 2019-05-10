eea57a053c93d052895f6ca8fdfaf57a3b4465f0
Cassels-null <42cassels@gmail.com>
2019-05-10 19:11:49 -0400

    Tweak badmovie algorithm

--- server/scribe.js
+++ server/scribe.js
@@ -12,7 +12,7 @@ var scribe = {
         })
     },
     getMovies : (req, res)=>{
-        axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=populerity.asc&vote_count=2&with_genres='+req.query.genre_id+'&api_key='+API_KEY)
+        axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=vote_avrage.asc&vote_count.gte=1&with_genres='+req.query.genre_id+'&api_key='+API_KEY)
         .then((result)=>{
             res.send(result.data.results);
         })
